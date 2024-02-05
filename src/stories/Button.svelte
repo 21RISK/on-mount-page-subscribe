b6d725b73887abb9fd8fad612b410941a2c56683
Alex Bjørlig <alexbjorlig@icloud.com>
2024-02-05 13:22:58 +0100

    page onMount not working

---
+++ src/stories/Button.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import './button.css';
+
+  /**
+   * Is this the principal call to action on the page?
+   */
+  export let primary = false;
+
+  /**
+   * What background color to use
+   */
+  export let backgroundColor: string | undefined = undefined;
+  /**
+   * How large should the button be?
+   */
+  export let size: 'small' | 'medium' | 'large' = 'medium';
+  /**
+   * Button contents
+   */
+  export let label: string = '';
+
+  $: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  $: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+      page.subscribe(v => console.log('new page!', v))
+  })
+</script>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+  {style}
+>
+  {label}
+</button>
+
